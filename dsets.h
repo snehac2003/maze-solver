b7c7838d790536bc80e28635fb5d0be5fb8cd37b
Sneha Chaliki <116612868+snehac2003@users.noreply.github.com>
2024-07-29 17:33:39 -0700

    Create dsets.h

---
+++ dsets.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+class DisjointSets
+{
+  public:
+    
+    void addelements(int num);
+
+    int find(int elem);
+
+    void setunion(int a, int b);
+    
+    int size(int elem);
+  private:
+    std::vector<int> d_vec;
+
+};
